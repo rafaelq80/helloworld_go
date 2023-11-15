32e11fe00a73eec0e1bc4a9855eee2ca03283a34
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-11-15 10:26:19 -0300

    Golang Hello World

---
+++ main.go
@@ -0,0 +1,19 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello World!</h1>"))
+	})
+	fmt.Println("Server is starting at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
